bda38c5fe7625498aa04d2738eed767a14c0b94c
Tariq,khan <tariqkhan85@gmail.com>
2017-08-09 13:15:05 +0000

    change apache cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,6 +4,13 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #install apache package
+
+if node ['platform_family'] == "rhel"
+	package = "httpd"
+elsif node ['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
 	package_name 'httpd'
 	action :install
@@ -14,3 +21,4 @@ service 'apache2' do
 	action [:start, :enable]
 end
 
+#include_recipe 'apache::website'
